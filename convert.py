52160bbbb8cf742e10dc771fe68653e2a30a8637
Aengus Walton <Aengus.Walton@blue-yonder.com>
2015-04-06 11:01:41 +0200

    BeautifulSoup seems to lowercase tag names

--- convert.py
+++ convert.py
@@ -6,31 +6,12 @@ import sys
 from bs4 import BeautifulSoup
 
 
-FIELDS = ['title', 'metaTitle', 'fullTitle', 'shortTitle', 'metaDescription',
-          'shortDescription', 'fullDescription', 'description']
+FIELDS = ['title', 'metatitle', 'fulltitle', 'shorttitle', 'metadescription',
+          'shortdescription', 'fulldescription', 'description']
 
 def main(directory):
     for filename in [f for f in os.listdir(directory) if f.lower().endswith('.xml')]:
-        template = u"""
-            <html><head><title></title><body>
-            <h1>title</h1>
-            <p>{title}</p>
-            <h1>metaTitle</h1>
-            <p>{metaTitle}</p>
-            <h1>fullTitle</h1>
-            <p>{fullTitle}</p>
-            <h1>shortTitle</h1>
-            <p>{shortTitle}</p>
-            <h1>metaDescription</h1>
-            <p>{metaDescription}</p>
-            <h1>shortDescription</h1>
-            <p>{shortDescription}</p>
-            <h1>fullDescription</h1>
-            <p>{fullDescription}</p>
-            <h1>description</h1>
-            <p>{description}</p>
-            </body></html>
-        """
+        template = u"""<html><head><title></title><body>"""
         with open(os.path.join(directory, filename), 'r') as f:
             xml = f.read()
         soup = BeautifulSoup(xml)
@@ -45,7 +26,9 @@ def main(directory):
                         pass
             if text.endswith(']]>'):
                 text = text[:-3]
-            template = template.replace('{%s}' % field, text)
+            template += u'<h1>{field}</h1><p>{text}</p>'.format(
+                field=field, text=text)
+        template += u'</body></html>'
         output_file = os.path.join(directory, filename.lower().replace('.xml', '.html'))
         with codecs.open(output_file, 'w', 'utf8') as f:
             f.write(template.encode('ascii', 'xmlcharrefreplace'))
